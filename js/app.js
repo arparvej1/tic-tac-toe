165fd037099280872eb180e5c08232b16dec16ad
arparvej1 <parvej8pro@gmail.com>
2024-02-26 09:07:24 +0600

    found winner

--- js/app.js
+++ js/app.js
@@ -1,26 +1,95 @@
 const allBox = document.getElementsByClassName('box');
-const selectedBox = [];
-let nineBox = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9'];
+const winner = document.getElementById('winner');
+// const selectedBox = [];
+let nineBox = ['b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'];
+let gameOn = 1;
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8]
+];
 
 for (const box of allBox) {
   box.addEventListener('click', function (e) {
-    if (!selectedBox.includes(e.target.id)) {
-      e.target.innerText = "X";
-      selectedBox.push(e.target.id);
-      nineBox = nineBox.filter(remove => remove !== e.target.id);
-      nineBox.length > 0 ? compTurn() : null;
-    }
+    e.target.innerText = 'X'; // '✕'
+    e.target.disabled = true;
+    nineBox = nineBox.filter(remove => remove !== e.target.id);
+    checkWinner();
+    gameOn === 1 ? (nineBox.length > 0 ? setTimeout(compTurn, 100) : null) : null;
   })
 };
 
-function compTurn() {
-  const index = Math.floor(Math.random() * nineBox.length);
-  const compSelId = nineBox[index];
-  document.getElementById(compSelId).innerText = 'O';
-  selectedBox.push(compSelId);
-  nineBox = nineBox.filter(remove => remove !== compSelId);
-  console.log(nineBox[index]);
-  console.log(selectedBox);
-  console.log(nineBox);
 
+function compTurn() {
+  const i = Math.floor(Math.random() * nineBox.length);
+  const comID = document.getElementById(nineBox[i]);
+  comID.innerText = 'O';
+  comID.disabled = true;
+  // selectedBox.push(nineBox[i]);
+  nineBox = nineBox.filter(remove => remove !== nineBox[i]);
+  checkWinner();
 }
+
+function disabledAllBox() {
+  for (const box of allBox) {
+    box.disabled = true;
+  }
+}
+
+function enabledAllBox() {
+  for (const box of allBox) {
+    box.disabled = false;
+    box.innerText = '';
+  }
+}
+
+function checkWinner() {
+  console.log("Check starting");
+  for (const pattern of winPatterns) {
+    let pos1Val = allBox[pattern[0]].innerText;
+    let pos2Val = allBox[pattern[1]].innerText;
+    let pos3Val = allBox[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos1Val === pos3Val) {
+        console.log("Winner", pos1Val);
+        winner.innerText = "Winner "+ pos1Val;
+        gameOn = 0;
+        disabledAllBox();
+      }
+    }
+  }
+}
+
+function resetGame() {
+  enabledAllBox();
+  gameOn = 1;
+  nineBox = ['b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'];
+}
+
+document.getElementById('reset-game').addEventListener('click', resetGame);
+
+
+
+
+// -----------------------
+// for (const box of allBox) {
+//   box.addEventListener('click', function (e) {
+//     if (!selectedBox.includes(e.target.id)) {
+//       e.target.innerText = 'X'; // '✕'
+//       e.target.disabled = true;
+//       selectedBox.push(e.target.id);
+//       nineBox = nineBox.filter(remove => remove !== e.target.id);
+//       checkWinner();
+//       if (gameOn === 1) {
+//         nineBox.length > 0 ? setTimeout(compTurn, 100) : null;
+//       }
+//     }
+//   })
+// };
